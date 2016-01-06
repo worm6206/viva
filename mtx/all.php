95e3663182ac40bf85bc69bd1804e7cfdc46169e
worm6206 <worm6206@gmail.com>
2016-01-06 10:21:59 -0500

    enhence mtx menu

--- mtx/all.php
+++ mtx/all.php
@@ -1,11 +1,31 @@
 <?php
 $dir    = './';
-$files = scandir($dir);
-unset($files[0]);
-unset($files[1]);
+// $files = scandir($dir);
+// unset($files[0]);
+// unset($files[1]);
+
+function scan_dir($dir) {
+    $ignored = array('.', '..', '.svn', '.htaccess');
+
+    $files = array();    
+    foreach (scandir($dir) as $file) {
+        if (in_array($file, $ignored)) continue;
+        $files[$file] = filesize($file);
+    }
+
+    arsort($files);
+    $files = array_keys($files);
+
+    return ($files) ? $files : false;
+}
+
+$files = scan_dir($dir);
+
 ?>
 
-<?php foreach($files as $key=>$value): 
+
+
+<?php foreach(array_reverse($files) as $key=>$value): 
 if (strpos($value,'mtx') !== false) {
 ?>
     <a target="main" href="./all.html?<?php echo $value; ?>"><?php echo "[".filesize($value). "]<br>" . $value ; ?></a><br>
