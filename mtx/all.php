410ad20afd66f17f72038552c66d6668d2ff6381
worm6206 <worm6206@gmail.com>
2016-01-06 00:47:03 -0500

    fix bug

--- mtx/all.php
+++ mtx/all.php
@@ -3,12 +3,10 @@ $dir    = './';
 $files = scandir($dir);
 unset($files[0]);
 unset($files[1]);
-// echo "$files[2]";
-echo '<span style="color:#F00;text-align:center;">Reload before drawing next graph</span>';
 ?>
 
 <?php foreach($files as $key=>$value): 
 if (strpos($value,'mtx') !== false) {
 ?>
-    <a target="main" href="./all.html#<?php echo $value; ?>"><?php echo "[".filesize($value). "]<br>" . $value ; ?></a><br>
+    <a target="main" href="./all.html?<?php echo $value; ?>"><?php echo "[".filesize($value). "]<br>" . $value ; ?></a><br>
     <?php } endforeach; ?>
