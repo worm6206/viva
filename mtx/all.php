eba2daafbb07abb3afb110f19cfccfc21d5e1236
worm6206 <worm6206@gmail.com>
2016-01-08 20:13:58 -0500

    testing halt

--- mtx/all.php
+++ mtx/all.php
@@ -30,7 +30,7 @@ $files = scan_dir($dir);
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 </head>
 <br>
-<a type="button" class="btn btn-primary btn-xs" role="button" target="_self" href="allp.php">Use Precompute</a>
+<a type="button" class="btn btn-primary btn-xs" role="button" target="_self" href="all.php">Use Precompute</a>
 <br>
 <br>
 
